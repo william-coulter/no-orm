7cf2b71e58a80cca083df0dffd3d619002b2823c
William Coulter <4williamcoulter4@gmail.com>
2025-06-18 21:32:48 +1000

    Start on functionality test

--- tests/test-docs-hero-example/functionality.ts
+++ tests/test-docs-hero-example/functionality.ts
@@ -1,1 +1,29 @@
-/** Tests that the functions from `model` actually works against a DB. */
+/** Tests that the functions from `model` execute without errors against the DB. */
+import * as PenguinsModel from "./expected/public/penguins/model";
+
+// STARTHERE: Connect me. Use env variables.
+const connection: any = {};
+
+const create = await PenguinsModel.create({
+  connection,
+  shape: {
+    name: "Willy the penguin",
+    species: "Adélie",
+    waddle_speed_kph: 0.5,
+  },
+});
+
+const read = await PenguinsModel.get({
+  connection,
+  id: create.id,
+});
+
+const update = await PenguinsModel.update({
+  connection,
+  newRow: { ...read, waddle_speed_kph: 0.6 },
+});
+
+const _delete = await PenguinsModel.delete({
+  connection,
+  id: update.id,
+});
