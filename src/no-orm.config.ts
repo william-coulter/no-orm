b79b6c9ca43ba27f59cf416001df959fb2f6c079
William Coulter <4williamcoulter4@gmail.com>
2025-06-22 13:27:42 +1000

    Change config to include JSDoc

--- src/no-orm.config.ts
+++ src/no-orm.config.ts
@@ -1,8 +1,28 @@
 import z from "zod";
 
+/**
+ * The configuration for the no-orm CLI tool.
+ */
+export type NoOrmConfig = {
+  /**
+   * A Postgres connection string that `no-orm` will read from to generate outputs.
+   * E.g `postgres://postgres:postgres@localhost:5432/postgres`.
+   */
+  postgres_connection_string: string;
+
+  /**
+   * The directory where `no-orm` will save its generated outputs.
+   */
+  output_directory: string;
+};
+
+/** The parser for `no-orm` config. */
 export const noOrmConfigSchema = z.object({
   postgres_connection_string: z.string(),
   output_directory: z.string(),
 });
 
-export type NoOrmConfig = z.infer<typeof noOrmConfigSchema>;
+/** Will fail compilation if the `NoOrmConfig` type and `noOrmConfigSchema` diverge. */
+assert<TypeEqualityGuard<NoOrmConfig, z.infer<typeof noOrmConfigSchema>>>();
+function assert<T extends never>() {}
+type TypeEqualityGuard<A, B> = Exclude<A, B> | Exclude<B, A>;
