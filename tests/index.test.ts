fd0253ccee465bf4249fe45d3f4398677c396074
William Coulter <4williamcoulter4@gmail.com>
2025-06-22 12:17:06 +1000

    Indent SQL less

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -98,14 +98,14 @@ describe("no-orm", () => {
     // Drop tables for next test.
     await client.query(`
       DO $$ DECLARE
-          r RECORD;
+        r RECORD;
       BEGIN
-          FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
-              EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-          END LOOP;
-          FOR r IN (SELECT typname FROM pg_type WHERE typnamespace = 'public'::regnamespace) LOOP
-              EXECUTE 'DROP TYPE IF EXISTS ' || quote_ident(r.typname) || ' CASCADE';
-          END LOOP;
+        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+          EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+        END LOOP;
+        FOR r IN (SELECT typname FROM pg_type WHERE typnamespace = 'public'::regnamespace) LOOP
+          EXECUTE 'DROP TYPE IF EXISTS ' || quote_ident(r.typname) || ' CASCADE';
+        END LOOP;
       END $$;
     `);
   });
