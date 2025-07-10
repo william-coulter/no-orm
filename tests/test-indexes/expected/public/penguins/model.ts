afe14f617c6b36fb9bbf3be683519e36c48ebab6
William Coulter <4williamcoulter4@gmail.com>
2025-07-10 19:08:42 +1000

    Agree on expected output

---
+++ tests/test-indexes/expected/public/penguins/model.ts
@@ -0,0 +1,199 @@
+import { type CommonQueryMethods, sql } from "slonik";
+import {
+  type Id,
+  type Row,
+  aliasColumns,
+  columnsFragment,
+  row,
+  tableFragment,
+} from "./table";
+
+type BaseArgs = { connection: CommonQueryMethods };
+
+export type Create = {
+  name: string;
+  species: string;
+  waddle_speed_kph: number;
+  favourite_snack: string | null;
+  date_of_birth: Date;
+};
+
+export type CreateManyArgs = BaseArgs & { shapes: Create[] };
+
+export async function createMany({
+  connection,
+  shapes,
+}: CreateManyArgs): Promise<readonly Row[]> {
+  const tuples = shapes.map((shape) => [
+    shape.name,
+    shape.species,
+    shape.waddle_speed_kph,
+    shape.favourite_snack,
+    shape.date_of_birth.toISOString(),
+  ]);
+
+  const query = sql.type(row)`
+    INSERT INTO ${tableFragment} (
+      name,
+      species,
+      waddle_speed_kph,
+      favourite_snack,
+      date_of_birth
+    )
+    SELECT name, species, waddle_speed_kph, favourite_snack, date_of_birth
+    FROM ${sql.unnest(tuples, ["text", "text", "numeric", "text", "timestamptz"])}
+      AS input(name, species, waddle_speed_kph, favourite_snack, date_of_birth)
+    RETURNING ${columnsFragment}`;
+
+  return connection.any(query);
+}
+
+export type CreateArgs = BaseArgs & { shape: Create };
+
+export async function create({ connection, shape }: CreateArgs): Promise<Row> {
+  const result = await createMany({ connection, shapes: [shape] });
+  return result[0];
+}
+
+export type GetManyArgs = BaseArgs & { ids: Id[] };
+
+export async function getMany({
+  connection,
+  ids,
+}: GetManyArgs): Promise<readonly Row[]> {
+  const query = sql.type(row)`
+    SELECT ${columnsFragment}
+    FROM ${tableFragment}
+    WHERE id = ANY(${sql.array(ids, "int4")})`;
+
+  return connection.any(query);
+}
+
+type GetArgs = BaseArgs & { id: Id };
+
+export async function get({ connection, id }: GetArgs): Promise<Row> {
+  const result = await getMany({ connection, ids: [id] });
+  return result[0];
+}
+
+export type Update = {
+  name: string;
+  species: string;
+  waddle_speed_kph: number;
+  favourite_snack: string | null;
+  date_of_birth: Date;
+} & { id: Id };
+
+export type UpdateManyArgs = BaseArgs & { newRows: Update[] };
+
+export function updateMany({
+  connection,
+  newRows,
+}: UpdateManyArgs): Promise<readonly Row[]> {
+  const tuples = newRows.map((newRow) => [
+    newRow.id,
+    newRow.name,
+    newRow.species,
+    newRow.waddle_speed_kph,
+    newRow.favourite_snack,
+    newRow.date_of_birth.toISOString(),
+  ]);
+
+  const query = sql.type(row)`
+    UPDATE ${tableFragment} AS t SET
+      name = input.name,
+      species = input.species,
+      waddle_speed_kph = input.waddle_speed_kph,
+      favourite_snack = input.favourite_snack,
+      date_of_birth = input.date_of_birth
+    FROM ${sql.unnest(tuples, [
+      "int4",
+      "text",
+      "text",
+      "numeric",
+      "text",
+      "timestamptz",
+    ])} AS input(id, name, species, waddle_speed_kph, favourite_snack, date_of_birth)
+    WHERE t.id = input.id
+    RETURNING ${aliasColumns("t")}`;
+
+  return connection.any(query);
+}
+
+type UpdateArgs = BaseArgs & { newRow: Update };
+
+export async function update({ connection, newRow }: UpdateArgs): Promise<Row> {
+  const result = await updateMany({ connection, newRows: [newRow] });
+  return result[0];
+}
+
+export type DeleteManyArgs = BaseArgs & { ids: Id[] };
+
+export async function deleteMany({
+  connection,
+  ids,
+}: DeleteManyArgs): Promise<void> {
+  const query = sql.type(row)`
+    DELETE FROM ${tableFragment}
+    WHERE id = ANY(${sql.array(ids, "int4")})`;
+
+  await connection.query(query);
+}
+
+type DeleteArgs = BaseArgs & { id: Id };
+
+async function _delete({ connection, id }: DeleteArgs): Promise<void> {
+  await deleteMany({ connection, ids: [id] });
+}
+
+export { _delete as delete };
+
+export type GetManyBySpeciesAndDateOfBirth = BaseArgs & {
+  tuples: { species: string; date_of_birth: Date }[];
+};
+
+export async function getManyBySpeciesAndDateOfBirth({
+  connection,
+  tuples,
+}: GetManyBySpeciesAndDateOfBirth): Promise<readonly Row[]> {
+  const speciesList = tuples.map((t) => t.species);
+  const dobList = tuples.map((t) => t.date_of_birth.toISOString());
+
+  return connection.any(sql.type(row)`
+    SELECT ${aliasColumns("t")}
+    FROM ${tableFragment} AS t
+    JOIN unnest(
+      ${sql.array(speciesList, "text")},
+      ${sql.array(dobList, "timestamptz")}
+    ) AS input(species, date_of_birth)
+    ON input.species = t.species AND input.date_of_birth = t.date_of_birth
+  `);
+}
+
+export type GetManyByNameArgs = BaseArgs & {
+  name: string[];
+};
+
+export async function getManyByName({
+  connection,
+  name,
+}: GetManyByNameArgs): Promise<readonly Row[]> {
+  return connection.any(sql.type(row)`
+    SELECT ${columnsFragment}
+    FROM ${tableFragment}
+    WHERE name = ANY(${sql.array(name, "text")})
+  `);
+}
+
+export type GetByNameArgs = BaseArgs & {
+  name: string;
+};
+
+export async function getByName({
+  connection,
+  name,
+}: GetByNameArgs): Promise<Row | null> {
+  // TODO: Ensure if unique and not a FK / branded type, we do this.
+  const result = await getManyByName({ connection, name: [name] });
+  return result[0] ?? null;
+}
---
+++ tests/test-indexes/functionality.ts
@@ -0,0 +1,131 @@
+/** Tests that the functions from `model` execute without errors against the DB. */
+import { pool } from "../slonik-test-connection";
+import * as PenguinsModel from "./expected/public/penguins/model";
+import * as FlightAttemptsModel from "./expected/public/flight_attempts/model";
+
+await pool.connect(async (connection) => {
+  const basePenguinProperties = {
+    waddle_speed_kph: 0.5,
+    favourite_snack: null,
+  };
+
+  const penguinCreate1 = await PenguinsModel.create({
+    connection,
+    shape: {
+      name: "Penguin Create 1",
+      species: "Adélie",
+      date_of_birth: new Date("2025-01-01T00:00:00+10:00"),
+      ...basePenguinProperties,
+    },
+  });
+
+  await PenguinsModel.create({
+    connection,
+    shape: {
+      name: "Penguin Create 2",
+      species: "Adélie",
+      date_of_birth: new Date("2026-01-01T00:00:00+10:00"),
+      ...basePenguinProperties,
+    },
+  });
+
+  await PenguinsModel.create({
+    connection,
+    shape: {
+      name: "Penguin Create 3",
+      species: "Emperor",
+      date_of_birth: new Date("2025-01-01T00:00:00+10:00"),
+      ...basePenguinProperties,
+    },
+  });
+
+  const penguinCreate4 = await PenguinsModel.create({
+    connection,
+    shape: {
+      name: "Penguin Create 4",
+      species: "Emperor",
+      date_of_birth: new Date("2026-01-01T00:00:00+10:00"),
+      ...basePenguinProperties,
+    },
+  });
+
+  // Test complicated tuple query.
+  const getManyBySpeciesAndDateOfBirthResult =
+    await PenguinsModel.getManyBySpeciesAndDateOfBirth({
+      connection,
+      tuples: [
+        {
+          species: "Adélie",
+          date_of_birth: new Date("2025-01-01T00:00:00+10:00"),
+        },
+        {
+          species: "Emperor",
+          date_of_birth: new Date("2026-01-01T00:00:00+10:00"),
+        },
+      ],
+    });
+
+  const expected = new Set([penguinCreate1.id, penguinCreate4.id]);
+
+  if (
+    !(
+      getManyBySpeciesAndDateOfBirthResult.length === expected.size &&
+      getManyBySpeciesAndDateOfBirthResult
+        .map((p) => p.id)
+        .every((id) => expected.has(id))
+    )
+  ) {
+    throw new Error(
+      `Returned getManyBySpeciesAndDateOfBirthResult does not equal expected. Returned: ${JSON.stringify(getManyBySpeciesAndDateOfBirthResult, null, 2)}`,
+    );
+  }
+
+  await PenguinsModel.getManyByName({
+    connection,
+    name: [penguinCreate1.name],
+  });
+
+  await PenguinsModel.getByName({
+    connection,
+    name: penguinCreate4.name,
+  });
+
+  await FlightAttemptsModel.createMany({
+    connection,
+    shapes: [
+      {
+        penguin: penguinCreate1.id,
+        method: "ski_jump",
+        attempted_at: new Date("2026-01-01T03:00:00+10:00"),
+        altitude_cm: 10,
+        success: false,
+        failure_reason: "Ski boots were too tight.",
+      },
+      {
+        penguin: penguinCreate4.id,
+        method: "glider",
+        attempted_at: new Date("2026-01-01T04:00:00+10:00"),
+        altitude_cm: 5,
+        success: false,
+        failure_reason: "Penguins don't have hands.",
+      },
+    ],
+  });
+
+  await FlightAttemptsModel.getManyByPenguin({
+    connection,
+    penguin: [penguinCreate1.id],
+  });
+
+  await FlightAttemptsModel.getByPenguin({
+    connection,
+    penguin: penguinCreate1.id,
+  });
+
+  await FlightAttemptsModel.getManyByFailureReason({
+    connection,
+    failure_reason: ["Ski boots were too tight."],
+  });
+});
+
+process.exit(0);
