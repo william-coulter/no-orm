8f317265d614a2f51a1edb261770970332d402fd
William Coulter <4williamcoulter4@gmail.com>
2025-07-23 21:15:00 +1000

    Ensure no regressions in testing

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -37,22 +37,22 @@ describe("no-orm", () => {
   const TESTS_DIR = path.join(__dirname);
   type TestCase = { name: string; directory: string };
   const testCases: TestCase[] = [
-    // {
-    //   name: "Example from the docs (hero section).",
-    //   directory: path.join(TESTS_DIR, "test-docs-hero-example"),
-    // },
+    {
+      name: "Example from the docs (hero section).",
+      directory: path.join(TESTS_DIR, "test-docs-hero-example"),
+    },
     {
       name: "Test handling of all supported Postgres types.",
       directory: path.join(TESTS_DIR, "test-type-parsing"),
     },
-    // {
-    //   name: "Test foreign key references",
-    //   directory: path.join(TESTS_DIR, "test-foreign-keys"),
-    // },
-    // {
-    //   name: "Test indexes",
-    //   directory: path.join(TESTS_DIR, "test-indexes"),
-    // },
+    {
+      name: "Test foreign key references",
+      directory: path.join(TESTS_DIR, "test-foreign-keys"),
+    },
+    {
+      name: "Test indexes",
+      directory: path.join(TESTS_DIR, "test-indexes"),
+    },
   ];
 
   afterEach(async () => {
