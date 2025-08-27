d9bc16ad828a061816a9f869e704889bfd61ddac
William Coulter <4williamcoulter4@gmail.com>
2025-08-27 21:49:56 +1000

    Add `STARTHERE`

--- src/config/drift-catcher.ts
+++ src/config/drift-catcher.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { NoOrmConfig, noOrmConfigSchema } from ".";
 
 /** Will fail compilation if the `NoOrmConfig` type and `noOrmConfigSchema` diverge. */
+// STARTHERE: There is drift. Then, test everything still works with no schema provided.
 assert<TypeEqualityGuard<NoOrmConfig, z.infer<typeof noOrmConfigSchema>>>();
 function assert<T extends never>() {}
 type TypeEqualityGuard<A, B> = Exclude<A, B> | Exclude<B, A>;
