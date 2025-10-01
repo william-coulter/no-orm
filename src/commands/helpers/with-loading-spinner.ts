9f329ce4de374b002e152a709b9150feeb7619a5
William Coulter <4williamcoulter4@gmail.com>
2025-10-01 21:14:23 +1000

    Write `init` command

---
+++ src/commands/helpers/with-loading-spinner.ts
@@ -0,0 +1,26 @@
+import { createSpinner } from "nanospinner";
+
+/** Perform `action` with a loading spinner that the user can see. */
+export async function withLoadingSpinner({
+  action,
+  spinnerText,
+  successMessage,
+  failureMessage,
+}: Args): Promise<void> {
+  const spinner = createSpinner(spinnerText);
+
+  try {
+    spinner.start();
+    await action();
+    spinner.success(successMessage);
+  } catch (e) {
+    spinner.error(`${failureMessage}: ${e}`);
+  }
+}
+
+type Args = {
+  action: () => Promise<any>;
+  spinnerText: string;
+  successMessage: string;
+  failureMessage: string;
+};
--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,7 +1,103 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+import { writeFile } from "fs/promises";
+
 import * as logger from "../logger";
+import { withLoadingSpinner } from "./helpers/with-loading-spinner";
 
 export async function run({}: RunArgs): Promise<void> {
-  logger.info("Welcome to `no-orm`!");
+  try {
+    console.log(chalk.bgBlue.white("Welcome to no-orm!"));
+    console.log();
+
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "configPath",
+        message: "Where would you like to store your config file?",
+        default: "no-orm.config.ts",
+      },
+    ]);
+
+    const { configPath } = answers;
+
+    await withLoadingSpinner({
+      action: () => buildEmptyConfig(configPath),
+      spinnerText: `Building config at ${configPath}`,
+      failureMessage: `Cannot build config at ${configPath}`,
+      successMessage: `Built config at ${configPath}`,
+    });
+
+    console.log();
+    console.log(`To get started, run:`);
+    console.log(
+      chalk.bgBlue.white(`no-orm generate --config-path ${configPath}`),
+    );
+
+    const logo = getLogoForWidth(process.stdout.columns);
+    console.log(chalk.blue(logo));
+  } catch (e) {
+    logger.error(`Could not init no-orm: ${e}`);
+    process.exit(1);
+  }
 }
 
 type RunArgs = {};
+
+async function buildEmptyConfig(path: string): Promise<void> {
+  const emptyConfig = `import { NoOrmConfig } from "no-orm";
+
+const config: NoOrmConfig = {};
+
+export default config;
+`;
+
+  await writeFile(path, emptyConfig);
+}
+
+function getLogoForWidth(width: number): string {
+  if (width < 50) {
+    return logoSmall;
+  } else {
+    return logoLarge;
+  }
+}
+
+const logoLarge = `
+            @@@@@@@@@@           
+          @@@@@@@@@@@@@@@        
+        @@%+===*%%%*+===%@       
+       @%-%%***%@@@@#***%@@      
+       @@@%. -%-%#%#. =%.#@@     
+       @#*@:.+=#@%%@*:=::%@      
+       @@#=*%%@%+===*@%%*+%      
+       @@@#   =@%@@@@%- .%@      
+      @@@@@*:  .=*#=.  :%@@@     
+     @@@@@@+. .       ..=@@@@    
+   @@@@@@@=    . .  .    =@@@@@  
+  @@@@@@@*  ..   .  .  ...*@@@@  
+ @@@@@@@@:    .   .  .    =@@@@@ 
+ @@@@@@@#...  . .  .  .  .:@@@@@@
+@@@@@@@@#.  .   .  .  .  .:@@@@@@
+@@@@@@@@%:  .  .  .  .  . =@@@@@@
+@@  @@@@@= .  .  .  .  . :#@@  @@
+     @@@@@=  .  .  .  . .#@@     
+      @@@@@%-.  .  . .:*@@@      
+       @@@@@@@@#***#%@@@%@@@@    
+     @@*==+*%@@@@@@@%*+====+%@   
+     @@**=====%@    @@@#**%@@@   
+       @@@@@@@@@        @@@      
+`;
+
+const logoSmall = `
+    @@@@@@    
+  @@+-*%-=%@  
+   @++#+#+#@  
+  @@%..-.:@@  
+ @@@:.....:@@ 
+@@@#.......%@@
+@@@@......:@@ 
+  @@@=:::#@   
+  %+=+@ @*+@  
+`;
